219ba517fce20afd0bda134bd89965e6aa435cf7
mohammadounegh <mohammad.ounegh@gmail.com>
2019-02-23 15:04:28 +0330

    fdg

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const  express=require("express");
-
+const morgan=require("morgan");
 const  app=express();
+const  bodyParser=require("body-parser");
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 //example of middleware
 // app.use((req,res,next)=>{
 //     res.status(200).json({
@@ -9,15 +14,30 @@ const  app=express();
 // });
 
 
+//routing system
+//products dept
 const productRoutes=require("./api/routes/products");
 
 app.use("/products",productRoutes);
 
-
+//orders dept
 const ordersRoutes=require("./api/routes/orders");
 
 app.use("/orders",ordersRoutes);
 
+app.use((req,res,next)=>{
+    const  error=new Error("not found");
+    error.status=404;
+    next(error);
+});
 
+app.use((error,req,res,next)=>{
+
+   res.status(error.status || 500);res.json({
+        error:{
+            message:error.message
+        }
+    })
+});
 
 module.exports=app;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http=require('http');
 const app=require('./app');
 
 //setting PORT
-const  port=process.env.PORT || 3000;
+// const  port=process.env.PORT || 3000;
 
 //setting app
 const server=http.createServer(app);
 
 //listening
-server.listen(port,function () {
-    console.log(`server stated at http://localhost:${port}`);
-});
+server.listen(3005);
+
+console.log(`server stated at http://localhost:3005`);
